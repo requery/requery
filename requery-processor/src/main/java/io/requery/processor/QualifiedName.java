bde5a35f8addbedc985deb467ee480f9ac440e7f
Nikhil Purushe <npurushe@gmail.com>
2016-03-19 12:12:20 -0700

    Resolve #49 Indicate incorrect class name in exception

--- requery-processor/src/main/java/io/requery/processor/QualifiedName.java
+++ requery-processor/src/main/java/io/requery/processor/QualifiedName.java
@@ -34,7 +34,7 @@ class QualifiedName implements Name {
             throw new IllegalArgumentException();
         }
         if (!SourceVersion.isIdentifier(className)) {
-            throw new IllegalArgumentException("invalid class name identifier");
+            throw new IllegalArgumentException("Invalid class name identifier: " + className);
         }
         this.packageName = packageName;
         this.className = className;
@@ -62,19 +62,19 @@ class QualifiedName implements Name {
         }
         packageName = packageBuilder.toString();
         if (Names.isEmpty(className)) {
-            throw new IllegalArgumentException("empty class name");
+            throw new IllegalArgumentException("Empty class name");
         }
         if (!SourceVersion.isIdentifier(className)) {
-            throw new IllegalArgumentException("invalid class name identifier");
+            throw new IllegalArgumentException("Invalid class name identifier: " + className);
         }
         this.className = className;
     }
 
-    public String packageName() {
+    String packageName() {
         return packageName;
     }
 
-    public String className() {
+    String className() {
         return className;
     }
 
