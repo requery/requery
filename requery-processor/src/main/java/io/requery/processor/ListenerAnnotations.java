54297001cf307d2ee451388ecd1b32ab3953acea
Nikhil Purushe <npurushe@gmail.com>
2016-05-06 20:38:19 -0700

    Reduce visibility fix warnings

--- requery-processor/src/main/java/io/requery/processor/ListenerAnnotations.java
+++ requery-processor/src/main/java/io/requery/processor/ListenerAnnotations.java
@@ -35,7 +35,7 @@ class ListenerAnnotations {
         return Stream.concat(annotationClasses(), jpaAnnotationClasses());
     }
 
-    static Stream<Class<? extends Annotation>> jpaAnnotationClasses() {
+    private static Stream<Class<? extends Annotation>> jpaAnnotationClasses() {
         return Stream.of(
             javax.persistence.PostLoad.class,
             javax.persistence.PostPersist.class,
@@ -46,7 +46,7 @@ class ListenerAnnotations {
             javax.persistence.PreUpdate.class);
     }
 
-    static Stream<Class<? extends Annotation>> annotationClasses() {
+    private static Stream<Class<? extends Annotation>> annotationClasses() {
         return Stream.of(
             PostLoad.class,
             PostInsert.class,
