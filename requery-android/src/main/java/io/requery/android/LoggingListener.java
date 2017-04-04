241ba9c80a11d5b68415a0be778cb2b05e382f35
Nikhil Purushe <npurushe@gmail.com>
2017-04-03 23:26:51 -0700

    Android fix typo in delete log message

--- requery-android/src/main/java/io/requery/android/LoggingListener.java
+++ requery-android/src/main/java/io/requery/android/LoggingListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 requery.io
+ * Copyright 2017 requery.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,7 +60,7 @@ public class LoggingListener implements StatementListener,
 
     @Override
     public void postDelete(Object entity) {
-        Log.i(tag, String.format("postDelete %s}", entity));
+        Log.i(tag, String.format("postDelete %s", entity));
     }
 
     @Override
