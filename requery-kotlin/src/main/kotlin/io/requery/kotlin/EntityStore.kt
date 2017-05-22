0ff5fab74edd1214e7a52ecd05c29b1e815bd93f
Nikhil Purushe <npurushe@gmail.com>
2017-05-22 00:24:58 -0700

    Add Kotlin specific documentation

--- requery-kotlin/src/main/kotlin/io/requery/kotlin/EntityStore.kt
+++ requery-kotlin/src/main/kotlin/io/requery/kotlin/EntityStore.kt
@@ -22,24 +22,107 @@ import java.util.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 
+/**
+ * The primary interface for interacting with [io.requery.Entity] objects. This interface supports the
+ * basic insert/update/delete operations.
+ */
 interface EntityStore<T : Any, out R> : Queryable<T>, AutoCloseable {
 
     override fun close()
+
+    /**
+     * Inserts the given entity. This entity must not have previously been inserted otherwise a
+     * [io.requery.PersistenceException] may be thrown.
+     */
     infix fun <E : T> insert(entity: E): R
+
+    /**
+     * Insert a collection of entities. This method may perform additional optimizations not
+     * present in the single element insert method.
+     */
     infix fun <E : T> insert(entities: Iterable<E>): R
+
+    /**
+     * Inserts the given entity returning the generated key after the entity is inserted. This
+     * entity must not have previously been inserted otherwise an [io.requery.PersistenceException]
+     * may be thrown.
+     */
     fun <K : Any, E : T> insert(entity: E, keyClass: KClass<K>): R
+
+    /**
+     * Insert a collection of entities returning the generated keys for the inserted entities in
+     * the order they were inserted.
+     */
     fun <K : Any, E : T> insert(entities: Iterable<E>, keyClass: KClass<K>): R
+
+    /**
+     * Update the given entity. If the given entity has modified properties those changes will be
+     * persisted otherwise the method will do nothing. A property is considered modified
+     * if its associated setter has been called, modifying the state of a property's content
+     * will not cause an update to happen.
+     */
     infix fun <E : T> update(entity: E): R
+
+    /**
+     * Updates a collection of entities. This method may perform additional optimizations not
+     * present in the single element update method.
+     */
     infix fun <E : T> update(entities: Iterable<E>): R
+
+    /**
+     * Upserts (insert or update) the given entity. Note that upserting may be an expensive
+     * operation on some platforms and may not be supported in all cases or platforms.
+     */
     infix fun <E : T> upsert(entity: E): R
+
+    /**
+     * Upserts (inserts or updates) a collection of entities. This method may perform additional
+     * optimizations not present in the single upsert method.
+     */
     infix fun <E : T> upsert(entities: Iterable<E>): R
+
+    /**
+     * Refresh the given entity. This refreshes the already loaded properties in the entity. If no
+     * properties are loaded then the default properties will be loaded.
+     */
     infix fun <E : T> refresh(entity: E): R
+
+    /**
+     * Refresh the given entity on specific attributes.
+     */
     fun <E : T> refresh(entity: E, vararg attributes: Attribute<*, *>): R
+
+    /**
+     * Refresh the given entities on specific attributes.
+     */
     fun <E : T> refresh(entities: Iterable<E>, vararg attributes: Attribute<*, *>): R
+
+    /**
+     * Refresh the given entity on all of its attributes including relational ones.
+     */
     fun <E : T> refreshAll(entity: E): R
+
+    /**
+     * Deletes the given entity from the store.
+     */
     infix fun <E : T> delete(entity: E): R?
+
+    /**
+     * Deletes multiple entities.
+     */
     infix fun <E : T> delete(entities: Iterable<E>): R?
+
+    /**
+     * Find an entity by the given key. This differs from selecting the key using a query in that
+     * the [io.requery.EntityCache] if available may be checked first for the object. If an entity is
+     * found in the cache it will be returned and potentially no query will be made.
+     */
     fun <E : T, K> findByKey(type: KClass<E>, key: K): R?
+
+    /**
+     * @return a {@link BlockingEntityStore} version of this entity store. If the implementation
+     * is already blocking may return itself.
+     */
     fun toBlocking(): BlockingEntityStore<T>
 }
 
--- requery-kotlin/src/main/kotlin/io/requery/kotlin/Queryable.kt
+++ requery-kotlin/src/main/kotlin/io/requery/kotlin/Queryable.kt
@@ -23,28 +23,100 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 
 /**
- * Interface for querying both [Entity] objects and selectable attributes of those objects.
+ * Interface for querying both [io.requery.Entity] objects and selectable attributes of those objects.
  *
  * @param <T> the base class or interface to restrict all entities that are queried to.
  */
 interface Queryable<T : Any> {
 
+    /**
+     * Initiates a query against a set of expression values return a specific entity type in a
+     * [io.requery.query.Result].
+     */
     infix fun <E : T> select(type: KClass<E>): Selection<out Result<E>>
+
+    /**
+     * Initiates a query against a set of attribute values return a specific entity type in a
+     * [io.requery.query.Result].
+     */
     fun <E : T> select(type: KClass<E>, vararg attributes: QueryableAttribute<E, *>): Selection<out Result<E>>
+
+    /**
+     * Initiates an insert operation for a type. After completing the expression call
+     * {@link Return#get()} to perform the operation. If the type has no generated key values the
+     * result is a tuple containing a single element with the number of rows affected by the
+     * operation. Otherwise if the type has generated keys those keys are returned as the result.
+     */
     infix fun <E : T> insert(type: KClass<E>): Insertion<out Result<Tuple>>
+
+    /**
+     * Initiates an insert operation for a type. After completing the expression call
+     * {@link Return#get()} to perform the operation. If the type has no generated key values the
+     * result is a tuple containing a single element with the number of rows affected by the
+     * operation. Otherwise if the type has generated keys those keys are returned as the result.
+     */
     fun <E : T> insert(type: KClass<E>, vararg attributes: QueryableAttribute<E, *>): InsertInto<out Result<Tuple>>
+
+    /**
+     * Initiates an update query for a type. The result is the number of rows
+     * affected by the call. Note that aggregate update queries will not affect existing entity
+     * objects in memory.
+     */
     infix fun <E : T> update(type: KClass<E>): Update<out Scalar<Int>>
+
+    /**
+     * Initiates an delete query for a type. Note that aggregate delete queries
+     * will not affect existing entity objects in memory or in the [io.requery.EntityCache] associated
+     * with the store.
+     */
     infix fun <E : T> delete(type: KClass<E>): Deletion<out Scalar<Int>>
+
+    /**
+     * Initiates a query to count the number of entities of a given type.
+     */
     infix fun <E : T> count(type: KClass<E>): Selection<out Scalar<Int>>
+
+    /**
+     * Initiates a query to count a given selection.
+     */
     fun count(vararg attributes: QueryableAttribute<T, *>): Selection<out Scalar<Int>>
+
+    /**
+     * Initiates a query against a set of expression values.
+     */
     fun select(vararg expressions: Expression<*>): Selection<out Result<Tuple>>
+
+    /**
+     * Initiates an update query against this data store. The result is the number of rows
+     * affected by the call. Note that aggregate update queries will not affect existing entity
+     * objects in memory.
+     */
     fun update(): Update<out Scalar<Int>>
+
+    /**
+     * Initiates a delete query against this data store. The result is the number of rows
+     * affected by the call. Note that aggregate update queries will not affect existing entity
+     * objects in memory.
+     */
     fun delete(): Deletion<out Scalar<Int>>
+
+    /**
+     * Executes a raw query against the data store.
+     */
     fun raw(query: String, vararg parameters: Any): Result<Tuple>
+
+    /**
+     * Executes a raw query against the data store mapping on to a specific entity type.
+     */
     fun <E : T> raw(type: KClass<E>, query: String, vararg parameters: Any): Result<E>
 }
 
 // property selection support
+
+/**
+ * Initiates a query against a set of property attribute values returning a specific entity type in a
+ * [io.requery.query.Result].
+ */
 inline fun <T : Any, reified E : T> Queryable<T>
         .select(type: KClass<E>, vararg properties: KProperty1<E, *>): Selection<out Result<E>> {
     val attributes: MutableSet<QueryableAttribute<E, *>> = LinkedHashSet()
@@ -52,6 +124,10 @@ inline fun <T : Any, reified E : T> Queryable<T>
     return select(type, *attributes.toTypedArray())
 }
 
+/**
+ * Initiates a query against a set of property attribute values returning values as tuples in a
+ * [io.requery.query.Result].
+ */
 inline fun <T : Any, reified E : T> Queryable<T>
         .select(vararg properties: KProperty1<E, *>): Selection<out Result<Tuple>> {
     val attributes: MutableSet<Expression<*>> = LinkedHashSet()
