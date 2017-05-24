8dc0a7c241507a41aeb7f0222ba9dee845fe0742
Nikhil Purushe <npurushe@gmail.com>
2017-05-23 23:24:27 -0700

    Resolve #563 Jackson collection types force the getter to be used

--- requery-jackson/src/main/java/io/requery/jackson/DeserializerModifier.java
+++ requery-jackson/src/main/java/io/requery/jackson/DeserializerModifier.java
@@ -21,6 +21,12 @@ import com.fasterxml.jackson.databind.DeserializationConfig;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.BeanDeserializer;
 import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.introspect.POJOPropertyBuilder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 class DeserializerModifier extends BeanDeserializerModifier {
 
@@ -33,5 +39,36 @@ class DeserializerModifier extends BeanDeserializerModifier {
             return new EntityBeanDeserializer(beanDeserializer, deserializer.getObjectIdReader());
         }
         return super.modifyDeserializer(config, beanDesc, deserializer);
+    }
+
+    @Override
+    public List<BeanPropertyDefinition> updateProperties(DeserializationConfig config,
+                                                         BeanDescription beanDesc,
+                                                         List<BeanPropertyDefinition> propDefs) {
+        List<BeanPropertyDefinition> definitions = super.updateProperties(config, beanDesc, propDefs);
+        List<BeanPropertyDefinition> remove = new ArrayList<>();
+        List<BeanPropertyDefinition> add = new ArrayList<>();
+        for (BeanPropertyDefinition definition : definitions) {
+            if (definition.hasGetter() &&
+                    Collection.class.isAssignableFrom(definition.getGetter().getRawType())) {
+
+                if (definition instanceof POJOPropertyBuilder) {
+                    POJOPropertyBuilder builder = (POJOPropertyBuilder) definition;
+                    builder = new POJOPropertyBuilder(builder, builder.getFullName()) {
+                        @Override
+                        public boolean hasField() {
+                            return false; // forces the getter to be used on the collection
+                        }
+                    };
+                    remove.add(definition);
+                    add.add(builder);
+                }
+            }
+        }
+
+        definitions.removeAll(remove);
+        definitions.addAll(add);
+
+        return definitions;
     }
 }
