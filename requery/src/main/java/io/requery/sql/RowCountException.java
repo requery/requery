2aecf3f4a6ec9fb8ea049f0c608b6f0dc98f1ec7
0xnm <0xnm@users.noreply.github.com>
2018-03-19 12:00:38 +0100

    Replace entity name to entity class in RowCountException ctor

--- requery/src/main/java/io/requery/sql/RowCountException.java
+++ requery/src/main/java/io/requery/sql/RowCountException.java
@@ -18,19 +18,22 @@ package io.requery.sql;
 
 import io.requery.PersistenceException;
 
+import javax.annotation.Nonnull;
+
 /**
  * Exception thrown when the affected row count of a executed statement doesn't match the value
  * expected.
  */
 public class RowCountException extends PersistenceException {
 
-    private final String entityName;
+    @Nonnull
+    private final Class<?> entityClass;
     private final long expected;
     private final long actual;
 
-    RowCountException(String entityName, long expected, long actual) {
-        super(entityName + ": expected " + expected + " row affected actual " + actual);
-        this.entityName = entityName;
+    RowCountException(@Nonnull Class<?> entityClass, long expected, long actual) {
+        super(entityClass.getSimpleName() + ": expected " + expected + " row affected actual " + actual);
+        this.entityClass = entityClass;
         this.expected = expected;
         this.actual = actual;
     }
@@ -50,9 +53,10 @@ public class RowCountException extends PersistenceException {
     }
 
     /**
-     * @return entity name affected
+     * @return entity class affected
      */
-    public String getEntityName() {
-        return entityName;
+    @Nonnull
+    public Class<?> getEntityClass() {
+        return entityClass;
     }
 }
