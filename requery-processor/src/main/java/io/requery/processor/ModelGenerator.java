2c0885961a2192aa133a7b4be8027da58c2989eb
masc <masc@disappear.de>
2018-05-14 19:52:39 +0200

    FIXED #774 Annotation processor generates model types in arbitrary order, breaking incremental compilation

--- requery-processor/src/main/java/io/requery/processor/ModelGenerator.java
+++ requery-processor/src/main/java/io/requery/processor/ModelGenerator.java
@@ -28,63 +28,60 @@ import io.requery.meta.EntityModelBuilder;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Modifier;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Generates a java class file containing one or more {@link EntityModel} information for classes
  * that were processed by the annotation processor. The models can be used to group together
  * persistable classes that may belong to different databases through the {@link Entity#model()}
  * field.
- *
  * @author Nikhil Purushe
  */
 class ModelGenerator implements SourceGenerator {
 
-    private final String packageName;
-    private final ProcessingEnvironment processingEnvironment;
-    private final Collection<? extends EntityDescriptor> entities;
+  private final String packageName;
+  private final ProcessingEnvironment processingEnvironment;
+  private final Collection<? extends EntityDescriptor> entities;
 
-    ModelGenerator(ProcessingEnvironment processingEnvironment,
-                   String packageName,
-                   Collection<? extends EntityDescriptor> entities) {
-        this.processingEnvironment = processingEnvironment;
-        this.packageName = packageName;
-        this.entities = entities;
+  ModelGenerator(ProcessingEnvironment processingEnvironment,
+                 String packageName,
+                 Collection<? extends EntityDescriptor> entities) {
+    this.processingEnvironment = processingEnvironment;
+    this.packageName = packageName;
+    this.entities = entities;
+  }
+
+  @Override
+  public void generate() throws IOException {
+    ClassName typeName = ClassName.get(packageName, "Models");
+    TypeSpec.Builder type = TypeSpec.classBuilder(typeName)
+            .addModifiers(Modifier.PUBLIC)
+            .addMethod(MethodSpec.constructorBuilder()
+                    .addModifiers(Modifier.PRIVATE).build());
+    CodeGeneration.addGeneratedAnnotation(processingEnvironment, type);
+
+    Map<String, Set<EntityDescriptor>> models = new HashMap<>();
+    entities.forEach(entity ->
+            models.computeIfAbsent(entity.modelName(), key -> new HashSet<>()).add(entity));
+
+    for (String model : models.keySet()) {
+      List<EntityDescriptor> types = new ArrayList<>(models.get(model));
+      // Sort types by name to avoid arbitrary generation order, breaking incremental compilation
+      types.sort(Comparator.comparing(it -> it.typeName().toString()));
+
+      FieldSpec.Builder field = FieldSpec.builder(ClassName.get(EntityModel.class),
+              model.toUpperCase(), Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL);
+      CodeBlock.Builder fieldType = CodeBlock.builder();
+      fieldType.add("new $T($S)\n", ClassName.get(EntityModelBuilder.class), model);
+
+      types.forEach(e -> fieldType.add(".addType($T.$L)\n",
+              ClassName.bestGuess(e.typeName().toString()), EntityGenerator.TYPE_NAME));
+
+      fieldType.add(".build()");
+      field.initializer("$L", fieldType.build());
+      type.addField(field.build());
     }
 
-    @Override
-    public void generate() throws IOException {
-        ClassName typeName = ClassName.get(packageName, "Models");
-        TypeSpec.Builder type = TypeSpec.classBuilder(typeName)
-                .addModifiers(Modifier.PUBLIC)
-                .addMethod(MethodSpec.constructorBuilder()
-                        .addModifiers(Modifier.PRIVATE).build());
-        CodeGeneration.addGeneratedAnnotation(processingEnvironment, type);
-
-        Map<String, Set<EntityDescriptor>> models = new HashMap<>();
-        entities.forEach(entity ->
-                models.computeIfAbsent(entity.modelName(), key -> new HashSet<>()).add(entity));
-
-        for (String model : models.keySet()) {
-            Set<EntityDescriptor> types = models.get(model);
-
-            FieldSpec.Builder field = FieldSpec.builder(ClassName.get(EntityModel.class),
-                    model.toUpperCase(), Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL);
-            CodeBlock.Builder fieldType = CodeBlock.builder();
-            fieldType.add("new $T($S)\n", ClassName.get(EntityModelBuilder.class), model);
-
-            types.forEach(e -> fieldType.add(".addType($T.$L)\n",
-                    ClassName.bestGuess(e.typeName().toString()), EntityGenerator.TYPE_NAME));
-
-            fieldType.add(".build()");
-            field.initializer("$L", fieldType.build());
-            type.addField(field.build());
-        }
-
-        CodeGeneration.writeType(processingEnvironment, typeName.packageName(), type.build());
-    }
+    CodeGeneration.writeType(processingEnvironment, typeName.packageName(), type.build());
+  }
 }
