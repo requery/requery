d1ced88e1a8df99234eba2b06754615bde331a0e
Jake K. Dodd <6845693+jakekdodd@users.noreply.github.com>
2019-06-13 00:07:15 -0700

    Enable Version annotation for use with methods (#878)

--- requery/src/main/java/io/requery/Version.java
+++ requery/src/main/java/io/requery/Version.java
@@ -21,13 +21,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Defines a version attribute for optimistic locking. Field type must be a integer or long.
  */
 @Documented
-@Target(FIELD)
+@Target({FIELD, METHOD})
 @Retention(RUNTIME)
 public @interface Version {
 }
